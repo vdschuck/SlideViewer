9e2a74d11d84b74538ef4f7a7e858885fe974618
Vinicius S <vinicius.schuck@live.com>
2016-06-01 20:48:59 -0300

    Add files via upload

--- PresentationWriter.java
+++ PresentationWriter.java
@@ -4,17 +4,28 @@ import java.io.*;
 
 public class PresentationWriter {
 
-    public PresentationWriter(String filename) throws Exception {
-
+    public PresentationWriter(Presentation p) throws Exception {
         try {
-            File f = new File("Contatos.txt");
-            FileWriter fr = new FileWriter(f, true); //true escrever sem sobrepor 
+            File f = new File("teste.txt");
+            FileWriter fr = new FileWriter(f); //true escrever sem sobrepor 
             PrintWriter out = new PrintWriter(fr, true);
+            gravaObjeto(f, p);
 
         } catch (Exception e) {
             System.out.println("Erro: " + e);
         }
 
-    }    
+    } 
+    
+    private static void gravaObjeto(File f, Presentation p) {
+        try {
+            FileOutputStream fos = new FileOutputStream(f);
+            ObjectOutputStream os = new ObjectOutputStream(fos);
+            os.writeObject(p);
+            os.close();
+        } catch (IOException e) {
+            System.out.println("Erro ao gravar objeto. Erro: " + e);
+        }
+    }
    
 }
