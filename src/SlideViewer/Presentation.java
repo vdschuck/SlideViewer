62ce082810f676e9de95073df3998dbbf99c4326
Gitbones <william3.npm@gmail.com>
2016-06-08 01:10:34 -0300

    Ajustado para as listas simplesmente encadeadas.

--- src/SlideViewer/Presentation.java
+++ src/SlideViewer/Presentation.java
@@ -1,45 +1,41 @@
 package SlideViewer;
 
-public class Presentation {
+public class Presentation<E> {
 
-    private Style style[];
-    private int cont;
+    private SinglyLinkedList<Style> style;
     private Footer foo;
-    private Slide[] slide;
+    private SinglyLinkedList<Slide> slide;
 
     //Quantidades de Styles que o presentation pode ter.
-    public Presentation(int cont, Slide[] s) {
-        style = new Style[cont];
-        this.slide = s;
+    public Presentation() {
+        style = new SinglyLinkedList();
+        slide = new SinglyLinkedList();
     }
 
     public void addStyle(Style st) {
         if (st != null) {
-            if (cont < style.length) {
-                style[cont] = st;
-                cont++;
-            }
+            style.insertLast(st);
         }
     }
 
-    public Slide[] getSlides() {
+    public SinglyLinkedList getSlides() {
         return slide;
     }
 
-    public void setSlides(Slide[] slide) {
-        this.slide = slide;
+    public void setSlides(Slide slide) {
+        this.slide.insertLast(slide);
     }
 
     public Slide getSlide(int idx) {
-        return this.slide[idx];
+        return slide.get(idx);
     }
 
     public void setSlide(Slide s, int idx) {
-        this.slide[idx] = s;
+        this.slide.insert(s, idx);
     }
 
-    public Style getStyle(int index) {
-        return style[index];
+    public Style getStyle(int index) {      
+        return style.get(index);
     }
 
     public Footer getFoo() {
