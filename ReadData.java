01ca2f4d9fc1add8ca6757eb893e28373c437f43
Gitbones <william3.npm@gmail.com>
2016-06-22 22:02:55 -0300

    Add files via upload

---
+++ ReadData.java
@@ -0,0 +1,70 @@
+package SlideViewer;
+
+import javax.swing.JOptionPane;
+
+public class ReadData {
+
+    public static Integer readInt(String valor) throws Exception {
+        String x;
+        do {
+            try {
+                x = JOptionPane.showInputDialog(valor);
+                if (x == null) {
+                    return 0;
+                } else {
+                    int j = Integer.valueOf(x);
+                    if (j >= 0 && j <= 6) {
+                        return j;
+                    } else {
+                        throw new Exception("Valor inválido! ");
+                    }
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Valor inválido! ");
+            }
+        } while (true);
+    }
+    
+    public static Integer readIntSub(String valor) throws Exception {
+        String x;
+        do {
+            try {
+                x = JOptionPane.showInputDialog(valor);
+                if (x == null) {
+                    return 0;
+                } else {
+                    int j = Integer.valueOf(x);
+                    if (j >= 0 && j <= 7) {
+                        return j;
+                    } else {
+                        throw new Exception("Valor inválido! ");
+                    }
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Valor inválido! ");
+            }
+        } while (true);
+    }
+    
+    public static Integer readIntSlide(String valor, int p) throws Exception {
+        String x;
+        do {
+            try {
+                x = JOptionPane.showInputDialog(valor);
+                if (x == null) {
+                    return 0;
+                } else {
+                    int j = Integer.valueOf(x);
+                    if (j >= 0 && j <= p) {
+                        return j;
+                    } else {
+                        throw new Exception("Valor inválido! ");
+                    }
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Valor inválido! ");
+            }
+        } while (true);
+    }
+
+}
