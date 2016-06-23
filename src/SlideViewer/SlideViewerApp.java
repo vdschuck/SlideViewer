1ea1b54bb60ac0405b77e984bb01e59b816809b4
Vinicius S <vinicius.schuck@live.com>
2016-06-22 22:13:12 -0300

    Add files via upload
    teste

--- src/SlideViewer/SlideViewerApp.java
+++ src/SlideViewer/SlideViewerApp.java
@@ -41,13 +41,7 @@ public class SlideViewerApp {
                             switch (sub) {
                                 case 1: {
                                     int pos = ReadData.readIntSlide("Posição do slide: ", pp.getSlides().numElements);
-                                    Title nTitulo = new Title(JOptionPane.showInputDialog("Digite o titulo que deseja"));
-                                    int estilo = ReadData.readIntSlide("Digite o código do estilo", pp.getStyles().numElements);
-                                    
-                                    Slide novoSlide = new Slide();
-                                    novoSlide.setStyle(pp.getStyle(estilo));
-                                    edit.addSlide(novoSlide, pos, pp);
-                                    edit.changeTitle(nTitulo, pos, pp);
+                                    edit.addSlide(pp.getSlide(pos), pos, pp);
                                     break;
                                 }
                                 case 2: {
@@ -70,8 +64,14 @@ public class SlideViewerApp {
                                     int pos = ReadData.readIntSlide("A posição do slide que deseja alterar o titulo: ", pp.getSlides().numElements);
                                     try{
                                         String titulo = JOptionPane.showInputDialog("Digite o titulo que deseja");
-                                        Title nTitle = new Title(titulo);
-                                        edit.changeTitle(nTitle, pos, pp);
+                                        if(titulo.length() > 57){
+                                            JOptionPane.showMessageDialog(null, "Digite um número de caracteres válido", "Erro de escrita", JOptionPane.ERROR_MESSAGE); 
+                                        }
+                                        else{
+                                            Title nTitle = new Title(titulo);
+                                            edit.changeTitle(nTitle, pos, pp);
+                                        }
+                                        
                                     }
                                     catch(Exception e){
                                          JOptionPane.showMessageDialog(null, "Verique as informações digitadas", "Erro na leitura", JOptionPane.ERROR_MESSAGE);
@@ -79,12 +79,32 @@ public class SlideViewerApp {
                                     break;
                                 }
                                 case 6: {
-                                    int pos = ReadData.readIntSlide("Slide que deseja adicionar texto: ", pp.getSlides().numElements);
-                                    String texto = JOptionPane.showInputDialog("Digite o texto que gostaria de adicionar");
-                                    String linhaPos = JOptionPane.showInputDialog("Digite onde gostaria a posição onde gostaria de colocar");
-                                    int posLinha = Integer.parseInt(linhaPos);
-                                    ListItem newItem = new ListItem(texto,posLinha);
-                                    edit.addElement(newItem, posLinha,pos, pp);
+                                    int page = ReadData.readIntSlide("Slide que deseja adicionar texto: ", pp.getSlides().numElements);
+                                    
+                                    String texto = JOptionPane.showInputDialog("Digite o texto que gostaria de adicionar"); 
+                                    
+                                    String topicoPos = JOptionPane.showInputDialog("O texto é 1-Item ou 2-Sub-item: ");
+                                    int topico = Integer.parseInt(topicoPos); 
+                                     
+                                    String simbolos = JOptionPane.showInputDialog("O texto é 1-numerado ou 2-simbolos: "); 
+                                    
+                                    Node<ListItem> current = pp.getSlide(page).getElem().head;
+                                   
+                                    if(topico == 2){                                       
+                                        String posSub = JOptionPane.showInputDialog("Digite a posição do item para o sub-item: "); 
+                                       int teste = Integer.parseInt(posSub);
+                                       
+                                       for(int i=0; i<teste; i++){
+                                           current = current.getNext();
+                                       }
+                                       edit.addElement(current.element, topico, page, pp);                                       
+                                       current.element.adicionarSupTopico(texto, current.element.getSubTopicos().numElements); 
+                                    
+                                    }else if (topico == 1){
+                                       ListItem newItem = new ListItem(texto, 0);
+                                       newItem.adicionarTopico(newItem, 0, texto);
+                                       pp.getSlide(page).addElement(newItem);
+                                    } 
                                     break;
                                 }
                             }
